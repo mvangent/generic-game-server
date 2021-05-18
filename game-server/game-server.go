3f0c226356fad8fcaafb929e30d0db4cb76378f4
Melvin van Gent <mvg@3yourmind.com>
2021-05-18 21:32:38 +0200

    add errors and logging

--- game-server/game-server.go
+++ game-server/game-server.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/mvangent/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Georg")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("Melvin")
+
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Print(message)
 }
