95f0690d38532c10d614cdac57ba7b020508ee51
Melvin van Gent <mvg@3yourmind.com>
2021-05-19 23:33:05 +0200

    introduce go slices and randomness

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,12 +3,24 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("no name was given to greet")
 	}
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	formats := []string{"Hi %v. Welcome", "So nice to see you %v!", "Honor to meet you %v"}
+
+	return formats[rand.Intn(len(formats))]
 }
